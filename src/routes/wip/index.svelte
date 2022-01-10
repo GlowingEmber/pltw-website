e2c9505ac06595d3825a98f8d4d9f25d709e324a
Ethan Hopkins <58455740+GlowingEmber@users.noreply.github.com>
2022-01-09 21:39:25 -0800

    pltw

--- src/routes/wip/index.svelte
+++ src/routes/wip/index.svelte
@@ -14,21 +14,71 @@
 
 <div class="pad-top">
 	<div class="pad-sides">
-		<div>
-			<h3 style="font-size: 0.8em; padding-bottom: 15px; padding-top: 15pt">August 25, 2021</h3>
-			<h1>No work in progress projects been added yet!</h1>
+		<div class="project" id="capstone">
+			<h2>Capstone Project</h2>
+			<h3 style="text-align: center; font-size: 0.8em; padding-top: 15px; color: red">★ Major Project</h3>
+			<h3 style="text-align: center; font-size: 0.8em; padding-top: 15px"><i>'Package Pirating Prevention'</i></h3>
+			<h3 style="text-align: center; font-size: 0.8em; padding-bottom: 15px; padding-top: 15px">Project Added Jan 6, 2021</h3>
+			<p style="margin: auto;"><a href="https://www.cnbc.com/2020/01/10/package-theft-how-amazon-google-others-are-fighting-porch-pirates.html">According to CNBC</a>, more than 1.7 million packages are stolen (or lost) EACH DAY in the United States, costing sellers billions of dollars each year. Our goal is to build a solution to decrease or limit the amount of package theft, and are currently developing ideas.</p>
+			<br>
+			<hr>
+			<br>
+			<div class="align">
+				<h4>Research Document</h4>
+				<br>
+				<p>This is our group's shared document for the research project! We're done with elements A-C so far.</p>
+				<br>
+				<a target="_blank" href="https://docs.google.com/document/d/1x-VyUiv4vz1Ljr4aU771L6Ak9uuqY74W3yeDSz1pVRA/edit?usp=sharing" style="font-weight: bold; font-size: 30px; padding-top: 25px; padding-bottom: 25px; color: #388eff;">View Document</a>
+				<br>
+				<br>
+			</div>
+			<hr>
+			<br>
+			<div class="align">
+				<h4>Powerpoint Presentation</h4>
+				<br>
+				<p>This is what we presented to the class. Our presentation includes data from two surveys we conducted regarding package theft, where we asked whether people have personally had packages stolen or know someone who has experienced package theft. We also analyzed prior solutions and concluded that, although solutions do exist to limit pirate theft, they are not widespread and are often too expensive.</p>
+				<br>
+				<a target="_blank" href="https://docs.google.com/presentation/d/11jOM80SCMRTaVfRnA9bv_celX67Zm7YlJvHi2WHEYVA/edit?usp=sharing" style="font-weight: bold; font-size: 30px; padding-top: 25px; padding-bottom: 25px; color: #388eff;">View Slides</a>
+				<br>
+				<br>
+			</div>
 		</div>
 			
 	</div>
 </div>
 
+
 <style>
-	p {
-		max-width: 10000pt;
+	a {
+		
+		font-weight: bold; 
+		color: #388eff;
+	}
+	a:hover {
+		color: #2980B9;
+	}
+	.project {
+		width: 80%;
+		margin: auto;
+	}
+	.align {
+		text-align: center;
+	}
+	h2 {
+		text-align: center;
+		padding-top: 10vh;
 	}
 	hr {
 		border: 1px solid lightgray;
 		border-radius: 1px;
+		margin: auto;
+		width: 70%;
+	}
+	p {
+		text-align: justify;
+		margin: auto;
+		text-justify: inter-word;
 	}
 	#hero {
 		height: 70vh;
@@ -46,11 +96,5 @@
        	-moz-user-select: none; /* Old versions of Firefox */
         -ms-user-select: none; /* Internet Explorer/Edge */
             user-select: none; /* Non-prefixed version, currently */
-	}
-
-	#hero-canvas {
-		position: absolute;
-		height: 500px;
-		width: 100%;
 	}
 </style>
